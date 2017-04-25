f283d8be4049d3bc8f5fedb7c33420b63af7f1b6
Aquamarren <marsreyesmatias@gmail.com>
2017-04-25 23:18:12 +0800

    Update dbconnect1.php

--- pages/dbconnect1.php
+++ pages/dbconnect1.php
@@ -1,7 +1,7 @@
 <?php
  define('HOST','localhost');
  define('USER','root');
- define('PASS','');
+ define('PASS','1234');
  define('DB','thriftyadmin');
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
