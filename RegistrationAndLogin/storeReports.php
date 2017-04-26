7775ce9b306faa5d6967c8790eebd65da9923b86
Aquamarren <marsreyesmatias@gmail.com>
2017-04-26 09:34:48 +0800

    Update storeReports.php

--- RegistrationAndLogin/storeReports.php
+++ RegistrationAndLogin/storeReports.php
@@ -11,19 +11,19 @@
  
 	require_once('dbConnect.php');	
  	
-	$result = mysqli_query($con,"SELECT *  FROM REPORTS 
+	$result = mysqli_query($con,"SELECT *  FROM reports 
 	WHERE UserID = '$userID' AND ExpenseID='$expenseID' AND ExpenseDate = CURDATE() LIMIT 1");
 	$numrows = mysqli_num_rows($result);
 	
 	
 	if($numrows < 1){
-			$sql2 = "INSERT INTO REPORTS (UserName, ExpenseID, CategoryName, ExpenseAmount,ExpenseDate,PercentIncrease, UserID) 
+			$sql2 = "INSERT INTO reports (UserName, ExpenseID, CategoryName, ExpenseAmount,ExpenseDate,PercentIncrease, UserID) 
 			VALUES('$userName', '$expenseID', '$categoryName', '$expenseAmount', '$expenseDate', '$percentIncrease', '$userID')";		
 			$result2 =mysqli_query($con, $sql2);
 	}
 	else if($numrows > 0){
 		//while($row=mysqli_fetch_assoc($result)){
-			$sql1 = "UPDATE REPORTS SET UserName='$userName', ExpenseID='$expenseID', CategoryName='$categoryName', 
+			$sql1 = "UPDATE reports SET UserName='$userName', ExpenseID='$expenseID', CategoryName='$categoryName', 
 			ExpenseAmount='$expenseAmount', ExpenseDate='$expenseDate', PercentIncrease='$percentIncrease' WHERE ExpenseID='$expenseID' AND UserID='$userID'";
 			$result1 =mysqli_query($con, $sql1);
 		//}
